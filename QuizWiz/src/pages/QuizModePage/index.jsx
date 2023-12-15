cefa4727fefea910d05d3a3187747340811d5a10
“yourGithubUsername” <your@emailadd.com>
2023-12-15 15:49:28 +0000

    Set all the routes

--- QuizWiz/src/pages/QuizModePage/index.jsx
+++ QuizWiz/src/pages/QuizModePage/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const QuizModePage = () => {
   return (
+    <>
     <div>QuizModePage</div>
+    <p><Link to="/test/topics/quiz/results" >Results</Link></p>
+    </>
   )
 }
 
--- QuizWiz/src/pages/TopicsPage/index.jsx
+++ QuizWiz/src/pages/TopicsPage/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const TopicsPage = () => {
   return (
+    <>
     <div>TopicsPage</div>
+    <p><Link to="/test/topics/quiz" >Quiz</Link></p>
+    </>
   )
 }
 
