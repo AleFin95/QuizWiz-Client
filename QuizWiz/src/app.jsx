68697611a10a43b8029870de1afbfbb87845b4fe
“yourGithubUsername” <your@emailadd.com>
2023-12-15 15:26:47 +0000

    Define routes

--- QuizWiz/src/app.jsx
+++ QuizWiz/src/app.jsx
@@ -5,23 +5,40 @@ import * as Pages from './pages'
 import { PageWrapper } from './components';
 import './assets/app.css'
 
-const App = () => {
-    return <>
-    <PageWrapper/>
+
+function App() {
+    return (
+      <>
+        <PageWrapper />
         <Routes>
-            <Route path="/" element={<Pages.HomePage />}/>
-              <Route path="/mynotes" element={<Pages.MyNotesPage />} />
-                 <Route path="/learn" element={<Pages.LearnPage />} />
-                 <Route path="/timer" element={<Pages.TimerPage />} />
-                 <Route path="/timer/addnotes" element={<Pages.AddNotesPage />} />
-                 <Route path="/test" element={<Pages.TestInstructionsPage />} />
-                 <Route path="/topics" element={<Pages.TopicsPage />} />
-                 <Route path="/topics/quiz" element={<Pages.QuizModePage />} />
-                 <Route path="/topics/quiz/results" element={<Pages.ResultsPage />} />
-             <Route path="*" element={<Pages.NotFoundPage />} />
+          <Route path="/" element={<Pages.HomePage />} />
+          <Route path="/mynotes" element={<Pages.MyNotesPage />} />
+  
+          <Route path="/learn/*" element={
+            <>
+              <Routes>
+                <Route path="/" element={<Pages.LearnPage />} />
+                <Route path="timer" element={<Pages.TimerPage />} />
+                <Route path="timer/addnotes" element={<Pages.AddNotesPage />} />
+              </Routes>
+            </>
+          } />
+  
+          <Route path="/test/*" element={
+            <> 
+            <Routes>
+              <Route path="/" element={<Pages.TestInstructionsPage />} />
+              <Route path="topics" element={<Pages.TopicsPage />} />
+              <Route path="topics/quiz" element={<Pages.QuizModePage />} />
+              <Route path="topics/quiz/results" element={<Pages.ResultsPage />} />
+            </Routes>
+            </> 
+          } />
+  
+          <Route path="*" element={<Pages.NotFoundPage />} />
         </Routes>
-        
-    </>
-};
+      </>
+    );
+  }
 
 export default App;
--- QuizWiz/src/pages/TimerPage/index.jsx
+++ QuizWiz/src/pages/TimerPage/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const TimerPage = () => {
   return (
+    <>
     <div>TimerPage</div>
+    <p><Link to="/learn/timer/addnotes" >Add your Notes</Link></p>
+    </>
   )
 }
 
