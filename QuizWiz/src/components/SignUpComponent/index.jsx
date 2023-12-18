1800c1da968814e527036a0144b5b84574d7a249
“yourGithubUsername” <your@emailadd.com>
2023-12-18 12:53:58 +0000

    fix forms

--- QuizWiz/src/components/SignUpComponent/index.jsx
+++ QuizWiz/src/components/SignUpComponent/index.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const SignUpComponent = () => {
   return (
-    <div>SignUpComponent</div>
+    <>
+    <h2>Account Registration</h2>
+    <form>
+      <input type="text" name="username" placeholder = "username"autoComplete="off"/>
+      <br/>
+      <input type="password" name="password" placeholder="password"autoComplete="off"/> 
+      <br/> 
+      <br/>
+      <input type="submit" value="Register"/>
+    </form>
+    </>
   )
+  
 }
 
 export default SignUpComponent
