2b31998f86b22f9d5eb4d0f8320e050b5a3e823e
“yourGithubUsername” <your@emailadd.com>
2023-12-18 12:41:59 +0000

    Fix sign up

--- QuizWiz/src/pages/loginPage/index.jsx
+++ QuizWiz/src/pages/loginPage/index.jsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../contexts'
+import {SignUpComponent} from "../../components"
 
 const Login = () => {
     const [inputValue, setInputValue] = useState ("")
+    const [showSignUp, setShowSignUp] = useState(false);
     const navigateTo = useNavigate ()
     const {setUser} = useAuth()
 
@@ -18,18 +20,22 @@ const Login = () => {
     }
 
     function handleSignUpClick() {
-        navigateTo("/signup")
+        setShowSignUp(!showSignUp)
     }
 
 
   return (
+    <>
     <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleInput} value={inputValue} placeholder='username' autoComplete="off"/>
         <input type="password"  placeholder="password" autoComplete="off"></input>
         <br/>
         <input type="submit" value="Log in"/>
         <p>No account? <button type = 'button' onClick={handleSignUpClick} > Sign up</button></p>
+    
     </form>
+    {showSignUp && <SignUpComponent />}
+    </>
   )
 }
 
