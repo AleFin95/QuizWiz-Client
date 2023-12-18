101fc4983e822e340a891ff455d0fdb3b4c89870
laplata247 <enquiriesmbp@gmail.com>
2023-12-18 15:18:13 +0000

    added script lines

--- QuizWiz/src/pages/HomePage/HomePage.test.jsx
+++ QuizWiz/src/pages/HomePage/HomePage.test.jsx
@@ -5,5 +5,26 @@ import { screen, render, cleanup } from '@testing-library/react';
 import matchers from '@testing-library/jest-dom/matchers';
 expect.extend(matchers);
 
-import Clicker from '.';
+import HomePage from './HomePage'; // Adjust the import path as needed
+
+describe('HomePage', () => {
+    it('renders correctly', () => {
+        render(
+            <Router>
+                <HomePage />
+            </Router>
+        );
+
+        // Check if the main heading is rendered
+        expect(screen.getByRole('heading', { name: /home/i })).toBeInTheDocument();
+
+        // Check if the links are rendered
+        expect(screen.getByRole('link', { name: /learn and take notes/i })).toBeInTheDocument();
+        expect(screen.getByRole('link', { name: /go to quiz/i })).toBeInTheDocument();
+
+        // Check if the links have correct href
+        expect(screen.getByRole('link', { name: /learn and take notes/i })).toHaveAttribute('href', '/learn');
+        expect(screen.getByRole('link', { name: /go to quiz/i })).toHaveAttribute('href', '/test');
+    });
+});
 
