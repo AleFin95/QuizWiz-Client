73806e1f213b5862659702f79d415ee0221a1a71
“yourGithubUsername” <your@emailadd.com>
2023-12-18 14:24:26 +0000

    Add log out functionality

--- QuizWiz/src/contexts/index.jsx
+++ QuizWiz/src/contexts/index.jsx
@@ -7,9 +7,13 @@ const AuthContext = createContext();
 export const AuthProvider =({children}) => {
     const [user, setUser] = useState();
 
+    const logOut = () => {
+        setUser(null);
+      };
+
 
     return (
-        <AuthContext.Provider value= {{user, setUser}}>
+        <AuthContext.Provider value= {{user, setUser, logOut}}>
         {children}
         </AuthContext.Provider>
     );
