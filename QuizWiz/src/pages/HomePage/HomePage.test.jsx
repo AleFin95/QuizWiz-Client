e29798479fe05d3137532367e5845d12cff0f1d2
laplata247 <enquiriesmbp@gmail.com>
2023-12-18 15:30:34 +0000

    written tests

--- QuizWiz/src/pages/HomePage/HomePage.test.jsx
+++ QuizWiz/src/pages/HomePage/HomePage.test.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
-import { describe, it, expect, beforeEach, afterEach } from 'vitest';
-import { screen, render, cleanup } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import HomePage from './index';
 
-import matchers from '@testing-library/jest-dom/matchers';
-expect.extend(matchers);
-
-import HomePage from './HomePage'; // Adjust the import path as needed
 
 describe('HomePage', () => {
-    it('renders correctly', () => {
-        render(
-            <Router>
-                <HomePage />
-            </Router>
-        );
+  it('renders correctly', () => {
+    render(<HomePage />);
 
-        // Check if the main heading is rendered
+      
         expect(screen.getByRole('heading', { name: /home/i })).toBeInTheDocument();
 
-        // Check if the links are rendered
+   
         expect(screen.getByRole('link', { name: /learn and take notes/i })).toBeInTheDocument();
         expect(screen.getByRole('link', { name: /go to quiz/i })).toBeInTheDocument();
 
-        // Check if the links have correct href
         expect(screen.getByRole('link', { name: /learn and take notes/i })).toHaveAttribute('href', '/learn');
         expect(screen.getByRole('link', { name: /go to quiz/i })).toHaveAttribute('href', '/test');
     });
 });
-
