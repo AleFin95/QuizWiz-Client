343ab220263361ae248b046fc60f05cfc6affc6e
laplata247 <enquiriesmbp@gmail.com>
2023-12-18 16:56:12 +0000

    commit

--- QuizWiz/src/pages/HomePage/HomePage.test.jsx
+++ QuizWiz/src/pages/HomePage/HomePage.test.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import * as matchers from '@testing-library/jest-dom/matchers';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import HomePage from './index';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { describe, expect, it } from 'vitest';
+import HomePage from '.';
 
-
+expect.extend(matchers);
 describe('HomePage', () => {
+  render(
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  );
+
   it('renders correctly', () => {
-    render(<HomePage />);
-
-      
-        expect(screen.getByRole('heading', { name: /home/i })).toBeInTheDocument();
-
-        expect(screen.getByRole('link', { name: /learn and take notes/i })).toBeInTheDocument();
-        expect(screen.getByRole('link', { name: /go to quiz/i })).toBeInTheDocument();
-
-        expect(screen.getByRole('link', { name: /learn and take notes/i })).toHaveAttribute('href', '/learn');
-        expect(screen.getByRole('link', { name: /go to quiz/i })).toHaveAttribute('href', '/test');
-    });
+    expect(screen.getByRole('heading', { name: /home/i })).toBeInTheDocument();
+    console.log('testing');
+    expect(
+      screen.getByRole('link', { name: /learn and take notes/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', { name: /go to quiz/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', { name: /learn and take notes/i })
+    ).toHaveAttribute('href', '/learn');
+    expect(screen.getByRole('link', { name: /go to quiz/i })).toHaveAttribute(
+      'href',
+      '/test'
+    );
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
