9f5a0bede735da4c9547208379d39cf790875d5b
“yourGithubUsername” <your@emailadd.com>
2023-12-18 15:07:44 +0000

    add test to homepage

--- QuizWiz/src/pages/HomePage/HomePage.test.jsx
+++ QuizWiz/src/pages/HomePage/HomePage.test.jsx
@@ -7,21 +7,3 @@ expect.extend(matchers);
 
 import Clicker from '.';
 
-
-describe("Clicker component", () => {
-
-    beforeEach(() => {
-        render(<Clicker />);
-    });
-
-    it("Displays a heading with appropriate text", () => {
-
-        const elem = screen.getByRole("heading");
-        expect(elem).toBeInTheDocument();
-        expect(elem.textContent).toBe("Clicker!");
-    })
-    afterEach(() => {
-        cleanup();
-    })
-
-});
