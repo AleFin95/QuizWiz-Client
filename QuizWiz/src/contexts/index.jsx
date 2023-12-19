c6b622ce3ab23aedf7aa2c76cbf86547ecbd5e8e
“yourGithubUsername” <your@emailadd.com>
2023-12-19 10:20:39 +0000

    Fix login page

--- QuizWiz/src/contexts/index.jsx
+++ QuizWiz/src/contexts/index.jsx
@@ -5,21 +5,10 @@ const AuthContext = createContext();
 
 
 export const AuthProvider =({children}) => {
-    const [user, setUser] = useState(null);
-
-    const logIn = (token) => {
-        setUser({ token });
-      };
-    
-
-    const logOut = () => {
-        localStorage.removeItem('token');
-        setUser(null);
-      };
-
+    const [token, setToken] = useState(null);
 
     return (
-        <AuthContext.Provider value= {{user, setUser, logIn, logOut}}>
+        <AuthContext.Provider value= {{token, setToken}}>
         {children}
         </AuthContext.Provider>
     );
