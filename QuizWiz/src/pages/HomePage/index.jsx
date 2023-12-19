266d2efaa4c81dcca98270c196d1c2a36c989155
laplata247 <enquiriesmbp@gmail.com>
2023-12-19 16:40:13 +0000

    working on homePage

--- QuizWiz/src/pages/HomePage/index.jsx
+++ QuizWiz/src/pages/HomePage/index.jsx
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom';
 import '../../assets/app.css';
 
 const HomePage = () => {
-    return
-     <main>
-        <h1>Home </h1>
-        <ul>
-            <li><Link to="/learn" >Learn and Take Notes</Link></li>
-            <li><Link to="/test" >Go to Quiz</Link></li>
-        </ul>
-    </main>
+    return (
+        <main>
+            <h1>Home </h1>
+            <ul>
+                <li><Link to="/learn">Learn and Take Notes</Link></li>
+                <li><Link to="/test">Go to Quiz</Link></li>
+            </ul>
+        </main>
+    );
 };
 
 export default HomePage;
