2ffa9ec9206f8050462bd89d1ac6f34c12ac039e
“yourGithubUsername” <your@emailadd.com>
2023-12-19 15:24:19 +0000

    Redirect topics page to quiz mode

--- QuizWiz/src/components/QuizInstructionsWrapper/index.jsx
+++ QuizWiz/src/components/QuizInstructionsWrapper/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import QuizInstructions from './QuizInstructions';
+import {QuizInstructions} from "../index";
 
 const QuizInstructionsWrapper = () => {
   const [showInstructions, setShowInstructions] = useState(false);
@@ -9,7 +9,7 @@ const QuizInstructionsWrapper = () => {
   };
 
   return (
-    <div className="quiz-instructions-wrapper">
+    <div>
       {showInstructions && <QuizInstructions onClose={toggleInstructions} />}
       <button onClick={toggleInstructions}>Open Instructions</button>
     </div>
