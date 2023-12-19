ec3319584e86414b0448e7263aa804641ff7833d
“yourGithubUsername” <your@emailadd.com>
2023-12-19 14:53:12 +0000

    Edit search bar by adding press enter functionality

--- QuizWiz/src/components/TopicsFilter/index.jsx
+++ QuizWiz/src/components/TopicsFilter/index.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+const TopicsFilter = ({ textFilter, setTextFilter, onEnterPress }) => {
+  const updateTextFilter = (e) => {
+    setTextFilter(e.target.value);
+  };
 
-const TopicsFilter = ({textFilter,setTextFilter, onEnterPress  }) => {
-    function updateTextFilter (e) {
-        setTextFilter(e.target.value);
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      onEnterPress();
     }
-  
-    function handleKeyDown(e) {
-      if (e.key === 'Enter') {
-        onEnterPress(textFilter);
-      }
-    }
-    return (
-    <div><label>Search a Topic:<input type="text" value={textFilter} onChange={updateTextFilter} onKeyDown={handleKeyDown}/></label></div>
-  )
-}
+  };
 
-export default TopicsFilter
+  return (
+    <div>
+      <label>
+        Search a Topic:
+        <input type="text" value={textFilter} onChange={updateTextFilter} onKeyDown={handleKeyDown} />
+      </label>
+    </div>
+  );
+};
+
+export default TopicsFilter;
