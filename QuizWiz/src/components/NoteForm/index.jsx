2ea6f347b5aeb0f13586ab63b0f2da1b12f2034c
“yourGithubUsername” <your@emailadd.com>
2023-12-20 10:19:11 +0000

    Add answers box component

--- QuizWiz/src/components/NoteForm/index.jsx
+++ QuizWiz/src/components/NoteForm/index.jsx
@@ -16,8 +16,10 @@ async function createNote(note) {
     body: JSON.stringify(note)
    
   };
-
-  const response = await fetch("http://localhost:3000/notes", options);
+console.log("options",options)
+  // const response = await fetch("http://localhost:3000/notes", options);
+  const response = await fetch("https://quizwiz-api.onrender.com/notes", options);
+  
 
   if (!response.ok) {
     
