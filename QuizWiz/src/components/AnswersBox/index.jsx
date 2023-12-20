e4ede4d44502bda6949105c68df7871f99f009a8
nine96as <ashrafsaid79@gmail.com>
2023-12-20 22:10:46 +0000

    Revamp logic, add style refs

--- QuizWiz/src/components/AnswersBox/index.jsx
+++ QuizWiz/src/components/AnswersBox/index.jsx
@@ -1,33 +1,64 @@
 import React, { useEffect, useState } from 'react';
 
-const Answers = ({ questionId }) => {
-  const [answers, setAnswers] = useState([]);
+const Answers = ({ answers, currentQuestionIndex }) => {
+  const [disabled, setDisabled] = useState(false);
+  const [correct, setCorrect] = useState(false);
+  const [currentAnswers, setCurrentAnswers] = useState([]);
 
   useEffect(() => {
-    async function fetchAnswers() {
-      try {
-        const response = await fetch(
-          `https://quizwiz-api.onrender.com/answers/${questionId}`
-        );
-        const data = await response.json();
-        console.log(data);
-        setAnswers(data);
-      } catch (error) {
-        console.error('Error fetching answers:', error);
+    const fetchCurrentAnswers = async () => {
+      let currArray = [];
+      for (
+        let i = currentQuestionIndex * 4;
+        i < currentQuestionIndex * 4 + 4;
+        i++
+      ) {
+        currArray.push(answers[i]);
       }
-    }
+      setCurrentAnswers(currArray);
+      setDisabled(false);
+    };
 
-    fetchAnswers();
-  }, [questionId]);
+    fetchCurrentAnswers();
+  }, [currentQuestionIndex]);
+
+  useEffect(() => {}, [disabled]);
+
+  const handleClick = (e, isCorrect) => {
+    if (isCorrect) {
+      e.target.classList.add('answer-correct');
+
+      setDisabled(true);
+    } else {
+      e.target.classList.add('answer-incorrect');
+
+      let answers = e.target.parentElement.children;
+
+      for (let i = 0; i < answers.length; i++) {
+        console.log(answers[i].value);
+        if (answers[i].value === 'true') {
+          console.log('access');
+          answers[i].classList.add('answer-correct');
+        }
+      }
+
+      setDisabled(true);
+    }
+  };
 
   return (
     <div>
-      <h2>Answers:</h2>
-      <ul>
-        {answers.map((answer) => (
-          <li key={answer._id}>
-            {answer.text} - {answer.isCorrect ? 'Correct' : 'Incorrect'}
-          </li>
+      <ul className='answers'>
+        {currentAnswers.map((answer) => (
+          <button
+            disabled={disabled}
+            value={answer.isCorrect}
+            className='answer'
+            key={answer._id}
+            onClick={(e) => handleClick(e, answer.isCorrect)}
+          >
+            {answer.text}
+          </button>
         ))}
       </ul>
     </div>
