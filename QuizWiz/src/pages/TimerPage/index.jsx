0fc72ab72d8b0e2bdbe96af5002c9ccd95314081
“yourGithubUsername” <your@emailadd.com>
2023-12-20 15:03:38 +0000

    Fix timer on both pages

--- QuizWiz/src/pages/TimerPage/index.jsx
+++ QuizWiz/src/pages/TimerPage/index.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import {useAuth} from "../../contexts"
-import {useNavigate} from 'react-router-dom' 
+import React from 'react';
 import {Timer} from '../../components'
+import {LearnInstructionsWrapper} from '../../components'
 
 const TimerPage = () => {
   
   return (
-
+    <>
     <Timer flag_page={"add_notes"} /> //Passing the flag depending on page we use 
+    <LearnInstructionsWrapper/>
+    </>
   );
 };
 
