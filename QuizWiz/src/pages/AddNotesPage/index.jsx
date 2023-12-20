29c9c65d7df8fae4dc9ad6445b5424c33620c024
laplata247 <enquiriesmbp@gmail.com>
2023-12-20 12:53:48 +0000

    working on add note

--- QuizWiz/src/pages/AddNotesPage/index.jsx
+++ QuizWiz/src/pages/AddNotesPage/index.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { LearnInstructionsWrapper } from '../../components';
-import NoteForm from '../../components/NoteForm';
-import { useAuth } from '../../contexts';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { LearnInstructionsWrapper } from "../../components";
+import NoteForm from "../../components/NoteForm";
+import { useAuth } from "../../contexts";
 
 function AddNotesPage() {
   const { state } = useLocation();
@@ -33,7 +33,7 @@ function AddNotesPage() {
 
   return (
     <>
-      <div className='addNotes'>
+      <div className="addNotes">
         <h1>Add Note</h1>
 
         <p>
