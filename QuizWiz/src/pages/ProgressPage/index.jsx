8041d266540f3e0f44c8af1589ac84f62b920948
Esham <Esham156@gmail.com>
2023-12-20 18:10:12 +0000

    add style to progress page

--- QuizWiz/src/pages/ProgressPage/index.jsx
+++ QuizWiz/src/pages/ProgressPage/index.jsx
@@ -7,29 +7,37 @@ function Progress() {
   const [scores, setScores]=useState([])
   
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token = localStorage.getItem('token');
-        const response = await fetch(`https://quizwiz-api.onrender.com/scores/token/${token}`);
+    // const fetchData = async () => {
+    //   try {
+    //     const token = localStorage.getItem('token');
+    //     const response = await fetch(`https://quizwiz-api.onrender.com/scores/token/${token}`);
 
-        if (response.status === 200) {
-          const data = await response.json();
-          const sortedData = data.sort((a, b) => b.value - a.value);
-          setScores(sortedData);
-        } else {
-          throw new Error('Failed to fetch scores');
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    //     if (response.status === 200) {
+    //       const data = await response.json();
+    //       const sortedData = data.sort((a, b) => b.value - a.value);
+    //       setScores(sortedData);
+    //     } else {
+    //       throw new Error('Failed to fetch scores');
+    //     }
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // };
 
-    fetchData();
+    // fetchData();
+    const data = [
+      { "subjectName": "Mathematics", "value": 95 },
+      { "subjectName": "Science", "value": 88 },
+      { "subjectName": "History", "value": 75 }
+    ]
+
+    setScores(data)
+    
   }, []);
 
 
   return (
-    <>
+    <div className='ProgressPage'>
         <h2>My Progress</h2>
         <div className="leaderboardContainer">
           <table className="leaderboardTable">
@@ -45,7 +53,7 @@ function Progress() {
             </tbody>
           </table>
         </div>
-    </>
+    </div>
   )
 }
 
