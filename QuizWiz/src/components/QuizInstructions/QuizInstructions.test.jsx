fb25ab28eb388d8c4939121b91ef1c45809ae507
Esham <Esham156@gmail.com>
2023-12-21 17:10:00 +0000

    fix bugs with quizInstructions tests

--- QuizWiz/src/components/QuizInstructions/QuizInstructions.test.jsx
+++ QuizWiz/src/components/QuizInstructions/QuizInstructions.test.jsx
@@ -1,72 +1,89 @@
 import * as matchers from '@testing-library/jest-dom/matchers';
-import { render, screen, cleanup, fireEvent} from '@testing-library/react';
-import { afterEach, beforeEach, describe, expect, it } from 'vitest';
-import QuizInstructions from '.';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
+import React from 'react';
+import { describe, expect, it, beforeEach } from 'vitest';
+import QuizInstructions from './index';
 import { MemoryRouter } from 'react-router-dom';
 
-
-
 expect.extend(matchers);
+
 describe('QuizInstructions', () => {
-    afterEach(() => {
-      cleanup();
+  beforeEach(() => {
+    cleanup();
+  });
+
+  // Renders the quiz instructions container with a visible icon.
+  it('should render the quiz instructions container with a visible icon', () => {
+    render(
+      <MemoryRouter>
+        <QuizInstructions />
+      </MemoryRouter>
+    );
+    const icon = screen.getByTestId('icon-faInfoCircle');
+    expect(icon).toBeInTheDocument();
+  });
+
+  it('Displays the Quiz Instructions content', () => {
+    render(
+      <MemoryRouter>
+        <QuizInstructions />
+      </MemoryRouter>
+    );
+
+    const quizInstructionsHeader = screen.getByRole('heading', {
+      name: 'Quiz Instructions',
+    });
+    const welcomeHeader = screen.getByRole('heading', {
+      name: 'Time to Challenge Yourself !',
     });
 
-    it('Displays the Quiz Instructions content', () => {
-      render(<QuizInstructions />);
-  
-      const quizInstructionsHeader = screen.getByRole('heading', {
-        name: "Quiz Instructions",
-      });
-      const welcomeHeader = screen.getByRole('heading', {
-        name: "Time to Challenge Yourself !",
-      });
-  
-      const selectTopicItem = screen.getByText(
-        'Select a Topic:'
-      );
-      const setTimerItem = screen.getByText(
-        'Time Limit:'
-      );
-      const progress = screen.getByText(
-        'Track Your Progress:'
-      );
-  
-      expect(quizInstructionsHeader).to.exist;
-      expect(welcomeHeader).to.exist;
-      expect(selectTopicItem).to.exist;
-      expect(setTimerItem).to.exist;
-      expect(progress).to.exist;
-    });
-  
-    it('Displays the icon and is clickable', () => {
-        render(<QuizInstructions />);
-        const icon = screen.getByTestId('icon-faInfoCircle');
-        expect(icon).toBeInTheDocument();
-        fireEvent.click(icon);
-      });
+    const selectTopicItem = screen.getByText('Select a Topic:');
+    const setTimerItem = screen.getByText('Time Limit:');
+    const progress = screen.getByText('Track Your Progress:');
 
-      it('renders the Link', () => {
-        render(<QuizInstructions />);
-        
-        const linkElement = screen.getByRole('link', { name: /My Notes/i });
-    
-        expect(linkElement).toBeInTheDocument();
-      });
-    
-      it('navigates to the correct route when clicked', () => {
-        render(
-          <MemoryRouter initialEntries={['/']}>
-            <QuizInstructions />
-          </MemoryRouter>
-        );
-      
-        // Use screen to query for the link element
-        const linkElement = screen.getByRole('link', { name: /My Notes/i });
-      
-        fireEvent.click(linkElement);
-      
-        // Check the current route after the click
-        expect(window.location.pathname).toBe('/mynotes');
-      });
-    });
+    expect(quizInstructionsHeader).toBeTruthy();
+    expect(welcomeHeader).toBeTruthy();
+    expect(selectTopicItem).toBeTruthy();
+    expect(setTimerItem).toBeTruthy();
+    expect(progress).toBeTruthy();
+  });
+
+  it('Displays the icon and is clickable', () => {
+    render(
+      <MemoryRouter>
+        <QuizInstructions />
+      </MemoryRouter>
+    );
+    const icon = screen.getByTestId('icon-faInfoCircle');
+    expect(icon).toBeInTheDocument();
+    fireEvent.click(icon);
+  });
+
+  it('renders the Link', () => {
+    render(
+      <MemoryRouter>
+        <QuizInstructions />
+      </MemoryRouter>
+    );
+
+    const linkElement = screen.getByRole('link', { name: /My Notes/i });
+
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('navigates to the correct route when clicked', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <QuizInstructions />
+      </MemoryRouter>
+    );
+
+    // Use screen to query for the link element
+    const linkElement = screen.getByRole('link', { name: /My Notes/i });
+
+    fireEvent.click(linkElement);
+
+    // Check the current route after the click
+    expect(window.location.pathname).toBe('/mynotes');
+  });
+});
