6675984bc7535bff58a0d6b1000a70f562e6fc03
“yourGithubUsername” <your@emailadd.com>
2023-12-21 11:24:34 +0000

    fix search bar

--- QuizWiz/src/components/LearnInstructions/index.jsx
+++ QuizWiz/src/components/LearnInstructions/index.jsx
@@ -24,7 +24,12 @@ const LearnInstructions = () => {
         }`}
       >
         <h2>Learn Instructions</h2>
-        <p>Insert your learn instructions here.</p>
+        <h3>Welcome to Your <br/> Personal Learning Journey!</h3>
+        <ol>
+          <li> <b>Create your own topics:</b> <br/>take notes and enhance your knowledge.</li>
+          <li> <b>Set a Learning Timer:</b> Boost your concentration <br/> by setting a timer for focused learning sessions.</li>
+          <li> <b>Prepare for Quizzes:</b> Your custom notes will <br/>serve as your study material for quizzes later on. </li>
+        </ol>
       </div>
     </div>
   );
